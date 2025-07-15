8697096c07e61ed0d5b807bf10a41a25c6f36b87
MindSmith53 <work.areeb01@gmail.com>
2025-07-15 22:53:08 +0530

    create server actions

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,12 @@
 import { inngest } from "@/lib/inngest/client";
-import { helloWorld } from "@/lib/inngest/functions";
+import { generateIndustryInsights } from "@/lib/inngest/function";
 import { serve } from "inngest/next";
 
-
+// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [
-        helloWorld, // <-- This is where you'll always add all your functions
+        /* your functions will be passed here later! */
+        generateIndustryInsights,
     ],
 });
